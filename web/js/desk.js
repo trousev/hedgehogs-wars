0f1f5b3bc62778f8b3f0ca443d1f976178665ff0
Alexander Trousevich <trousev@yandex.ru>
2012-10-26 22:05:10 +0400

    implemented some tasty drawings

--- web/js/desk.js
+++ web/js/desk.js
@@ -1,6 +1,10 @@
 var Desk = function(desk_id)
 {
   var div = $(desk_id);
+  var hedgehogs = {};
+  var hedgehog_count = 0;
+  var cabbage_count = 0;
+  
   cell_id = function(x,y)
   {
     return "desk_cell_"+x+"_"+y; 
@@ -8,15 +12,72 @@ var Desk = function(desk_id)
   
   this.init = function(width, height)
   {
-    html = "";
+    var html = "";
     for(var x=0; x<width; x++)
       for(var y=0; y<height; y++)
       {
-        cx = 50*x+1;
-        cy = 50*y+1;
-        html += '<div id="cell1" style="position: absolute; left: '+cx+'px; top: '+cy+'px; width: 48px; height: 48px; color:black; background-color:black;"></div>';
+        var cx = Desk.cell_width *x;
+        var cy = Desk.cell_height *y;
+        html += '<div id="'+cell_id(x,y)+'" class="deskcell" style="position: absolute; left: '+cx+'px; top: '+cy+'px; width: '+Desk.cell_width +'px; height: '+Desk.cell_height +'px;"></div>';
       }
+    html += '<div id="hedgehogs" style="position: absolute; left:0px; top: 0px;" ></div>';
+    html += '<div id="cabbage" style="position: absolute; left:0px; top: 0px;" ></div>';
     div.html(html);
   }
-  
+  this.place = function(object_name, x, y)
+  {
+    $("#"+cell_id(x,y)).html('<div class="desk_'+object_name+'" style="position: absolute; left: 0px; top: 0px; width: '+Desk.cell_width +'px; height: '+Desk.cell_height +'px;""></div>');
+  }
+  this.hedgehog_create = function(name, x,y)
+  {
+    hedgehogs[name] = {"name": name, "x": x, "y": y, "no" : hedgehog_count};
+    var html = $("#hedgehogs").html();
+    var cx = Desk.cell_width  *x;
+    var cy = Desk.cell_height *y;
+    hedgehog_count += 1;
+    html += '<div id="hh_'+hedgehogs[name]["no"]+'" class=\"hedgehog alive\" style="position: absolute; left: '+cx+'px; top: '+cy+'px; width: '+Desk.cell_width +'px; height: '+Desk.cell_height +'px;">#'+hedgehogs[name]["no"]+'</div>';
+    $("#hedgehogs").html(html);
+  }
+  this.hedgehog_move = function(name, dx,dy)
+  {
+    hedgehogs[name]["x"] += dx;
+    hedgehogs[name]["y"] += dy;
+    var no = hedgehogs[name]["no"];
+    $("#hh_"+no).animate({
+          left: '+='+Desk.cell_width*dx,
+          top: '+='+Desk.cell_height*dy
+    },500);
+  }
+  this.hedgehog_kill = function(name)
+  {
+    var no = hedgehogs[name]["no"];
+    $("#hh_"+no).animate({
+      opacity: 0.3
+    },500);
+    $("#hh_"+no).rotate({angle: 0, animateTo: 180, duration: 300});
+    
+    $("#hh_"+no).addClass("dead");
+    $("#hh_"+no).removeClass("alive");
+  }
+  this.cabbage = function(from_x, from_y, to_x, to_y)
+  {
+    var html = $("#cabbage").html();
+    var cx = Desk.cell_width  *from_x;
+    var cy = Desk.cell_height *from_y;
+    cabbage_count += 1;
+    cid = 'cabbage_'+cabbage_count;
+    html += '<div id="'+cid+'" class=\"cabbage flying\" style="position: absolute; left: '+cx+'px; top: '+cy+'px; width: '+Desk.cell_width +'px; height: '+Desk.cell_height +'px;"></div>';
+    $("#cabbage").html(html);
+    $("#"+cid).rotate({angle: 0, animateTo: 900, duration: 1000});
+    $("#"+cid).animate({
+      left: "+="+Desk.cell_width * (to_x - from_x),
+      top: "+="+Desk.cell_height * (to_y - from_y)
+    },1000, function(){
+      $("#cabbage").remove();
+    });
+    
+  }
 };
+
+Desk.cell_width = 32
+Desk.cell_height = 32
